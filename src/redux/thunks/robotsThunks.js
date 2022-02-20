ff9a5585a7a358dc2d8c23c6607a22ef6fed01a2
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-20 16:40:55 +0100

    corrected loadRobotsListThunk in robotsThunks.js

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -3,5 +3,5 @@ import { loadRobotsListAction } from "../actions/actionsCreators";
 export const loadRobotsListThunk = async (dispatch) => {
   const response = await fetch(process.env.REACT_APP_API_ROBOTSAPI);
   const robots = await response.json();
-  dispatch(loadRobotsListAction(robots));
+  dispatch(loadRobotsListAction(robots.robots));
 };
