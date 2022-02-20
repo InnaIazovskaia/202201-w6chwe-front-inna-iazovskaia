ae3e4b66c0734ad021befc8360c7692648185804
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-20 11:43:54 +0100

    created server.js and handlers.js

---
+++ src/redux/mocks/handlers.js
@@ -0,0 +1,27 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get(process.env.REACT_APP_API_ROBOTSAPI),
+  (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          name: "Bibi",
+          image: "",
+          speed: 1,
+          strength: 2,
+          "date of creation": "2022-02-18",
+        },
+        {
+          id: 2,
+          name: "Bobo",
+          image: "",
+          speed: 6,
+          strength: 2,
+          "date of creation": "2022-02-18",
+        },
+      ])
+    ),
+];
